a1f4a89c178e59cff25ae5bc308ed79e2a3d9438
znp900714 <m15212413408@163.com>
2018-11-24 21:49:45 +0800

    添加请求响应报文到测试报告中

--- src/test/java/com/znp/apiautotest/api/cstusercontroller/signin/testcase/SignInTest.java
+++ src/test/java/com/znp/apiautotest/api/cstusercontroller/signin/testcase/SignInTest.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.znp.apiautotest.api.cstusercontroller.signin.bean.SignInRequ;
 import com.znp.apiautotest.api.cstusercontroller.signin.bean.SignInResp;
 import com.znp.apiautotest.api.tools.HttpTools;
+import io.qameta.allure.Allure;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -17,10 +18,12 @@ public class SignInTest {
         String requJson = JSON.toJSONString(signInRequ,true);
         System.out.println("--------------------登录请求报文--------------------");
         System.out.println(requJson);
+        Allure.addAttachment("请求报文", requJson);
         String url = "http://qa.guoyasoft.com:8080/user/login";
         String respJson = HttpTools.doPostByJson(url,requJson,"utf-8");
         System.out.println("--------------------登录响应报文--------------------");
         System.out.println(respJson);
+        Allure.addAttachment("响应报文", respJson);
         SignInResp signInResp = JSON.parseObject(respJson,SignInResp.class);
         Assert.assertEquals(signInResp.getRespCode(),"0000");
     }
--- src/test/java/com/znp/apiautotest/api/cstusercontroller/signup/testcase/SignUpTest.java
+++ src/test/java/com/znp/apiautotest/api/cstusercontroller/signup/testcase/SignUpTest.java
@@ -6,6 +6,7 @@ import com.znp.apiautotest.api.cstusercontroller.signup.bean.SignUpResp;
 import com.znp.apiautotest.api.tools.HttpTools;
 import com.znp.apiautotest.api.tools.PinYinTools;
 import com.znp.apiautotest.api.tools.RandomTools;
+import io.qameta.allure.Allure;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -26,10 +27,12 @@ public class SignUpTest {
         String requJson = JSON.toJSONString(signUpRequ,true);
         System.out.println("--------------------注册请求报文--------------------");
         System.out.println(requJson);
+        Allure.addAttachment("请求报文", requJson);
         String url = "http://qa.guoyasoft.com:8080/user/signup";
         String respJson = HttpTools.doPostByJson(url,requJson,"utf-8");
         System.out.println("--------------------注册响应报文--------------------");
         System.out.println(respJson);
+        Allure.addAttachment("响应报文", respJson);
         SignUpResp signUpResp = JSON.parseObject(respJson,SignUpResp.class);
         Assert.assertEquals(signUpResp.getRespBase().getRespCode(),"0000");
     }
